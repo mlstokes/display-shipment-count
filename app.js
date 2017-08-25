8d65d6168fc637d291413ab7ab30162b188acb08
Macon Stokes <citostokes@gmail.com>
2017-08-25 07:14:05 -0500

    gulp setup complete: code enforcement, css/js injection via bower and gulp-inject, nodemon to restart server automatically.  End of basic Development Environment setup.

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+//if PORT exists in the environment then use it, if not, use 5000
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function (req, res) {
-  res.send('Welcome to the Amp Dashboard');
+    res.send('Welcome to the Amp Dashboard');
 });
 
 app.get('/shipments', function (req, res) {
-  res.send('Shipment Data Goes Here');
+    res.send('Shipment Data Goes Here');
 });
 
 app.listen(port, function (err) {
-  console.log('running server on port ' + port);
+    console.log('running server on port ' + port);
 });
