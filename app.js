03322bf7a77fb9dfb1742c43ebbac576da9d2205
Macon Stokes <citostokes@gmail.com>
2017-08-30 08:53:56 -0500

    added queries.json, updated files to reflect that abstraction.  Added query data to Shipments route.  Orders route now broken - needs to be changed to reflect same changes as Shipments route.  Still need to figure out how to bring data into chart.js graphs in templates.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 var _ = require('underscore');
+var sql = require('mssql');
+var mssqlConfig = require('./src/data/mssqlConfig.json');
+var queries = require('./src/data/queries.json');
+
+sql.connect(mssqlConfig, function (err) {
+    if (err) { console.log(err); } else { console.log('mssql connected'); }
+});
 
 //if PORT exists in the environment then use it, if not, use 5000
 var port = process.env.PORT || 5000;
@@ -8,7 +15,7 @@ var port = process.env.PORT || 5000;
 var navData = require('./src/data/navData.json');
 var activityData = require('./src/data/activityData.json');
 
-var shipmentsRouter = require('./src/routes/shipmentsRouter')(navData, activityData);
+var shipmentsRouter = require('./src/routes/shipmentsRouter')(navData, activityData, queries);
 var ordersRouter = require('./src/routes/ordersRouter')(navData, activityData);
 
 app.use(express.static('public'));
@@ -21,10 +28,21 @@ app.use('/Shipments', shipmentsRouter);
 app.use('/Orders', ordersRouter);
 
 app.get('/', function (req, res) {
-    res.render('index', {
-                        title: 'Amplifier Dash',
-                        nav: navData,
-                        activityDataSummary: activityData});
+
+    console.log(queries.summaryQuery);
+    var summaryData = new sql.Request();
+
+    var queryData = summaryData.query(queries.summaryQuery,
+      function (err, recordset) {
+            var queryData = recordset.recordset;
+            console.log(queryData);
+            res.render('index', {
+                                    title: 'Amplifier Dash',
+                                    nav: navData,
+                                    activityDataSummary: queryData});
+            return queryData;
+        });
+
 });
 
 app.listen(port, function (err) {
--- src/routes/shipmentsRouter.js
+++ src/routes/shipmentsRouter.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var shipmentsRouter = express.Router();
 var _ = require('underscore');
+var sql = require('mssql');
 
-var router = function (navData, activityData) {
+var router = function (navData, activityData, queries) {
     shipmentsRouter.route('/')
       .get(function (req, res) {
-        res.render('activity', {
-                                title: 'Shipments',
-                                nav: navData,
-                                activityDataSummary: _.where(activityData, {Label: 'Shipments'})
-                            });
+        var request = new sql.Request();
+
+        request.query(queries.shipmentsQuery,
+                      function(err, recordset) {
+                        console.log(recordset.recordset);
+                        var shipmentsData = recordset.recordset;
+                        console.log(_.map(shipmentsData, _.property('Activities')));
+
+                        res.render('activity', {
+                                                title: 'Shipments',
+                                                nav: navData,
+                                                activityDataSummary: _.where(activityData, {Label: 'Shipments'}),
+                                                activityData: shipmentsData
+                                            });
+                      });
+
     });
 
     return shipmentsRouter;
